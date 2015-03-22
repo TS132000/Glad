3c5a2e55196d45e5c69c4f27733ab0a1ad4393c2
michealhan <hanjianhua2012@sjtu.edu.cn>
2015-03-22 16:17:06 +0800

    E is ok

--- GLAD.cpp
+++ GLAD.cpp
@@ -1,8 +1,114 @@
 #include<iostream>
+#include<fstream>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include<windows.h>
 using namespace std;
+double item[42624],person[60],test,test2,zj[42625];
+
+double gaussrand()
+{
+    static double V1, V2, S;
+    static int phase = 0;
+    double X;
+    V1=0.0;
+    V2=0.0;
+    S=0.0;
+    X=0.0;
+    phase = 0;
+    //srand(time(0));
+    if ( phase == 0 ) {
+        do {
+            double U1 = (double)rand() / RAND_MAX;
+            double U2 = (double)rand() / RAND_MAX;
+
+            V1 = 2 * U1 - 1;
+            V2 = 2 * U2 - 1;
+            S = V1 * V1 + V2 * V2;
+        } while(S >= 1 || S == 0);
+
+        X = V1 * sqrt(-2 * log(S) / S);
+    } else
+        X = V2 * sqrt(-2 * log(S) / S);
+
+    phase = 1 - phase;
+
+    return X;
+}
+
+int Initialize()
+{
+    ifstream in("question_full_data_b.txt");
+    ofstream out("question_full_data_E_TEST.txt");
+    char buffer[256];
+    double data_true,data_false;
+    int temp,a,b,c;
+    temp=1;
+    data_false=1;
+    data_true=1;
+    while(!in.eof())
+    {
+        in.getline(buffer,20);
+        sscanf(buffer,"%d %d %d",&a,&b,&c);
+        if (temp!=a)
+        {
+            double judge_item_true;
+            judge_item_true=data_true/(data_false+data_true);
+            zj[temp]=judge_item_true;
+            temp=a;
+            data_false=1;
+            data_true=1;
+        }
+        if (temp==a)
+        {
+            if (c==1)
+            {
+                data_true*=1/(1+exp(-item[a]*person[b]));
+                data_false*=(1-1/(1+exp(-item[a]*person[b])));
+            }
+            else
+            {
+                data_false*=1/(1+exp(-item[a]*person[b]));
+                data_true*=(1-1/(1+exp(-item[a]*person[b])));
+            }
+        }
+        if (a==42624)
+        {
+            zj[a]=data_true/(data_false+data_true);
+        }
+
+    }
+    for (int l=1;l<42625;l++)
+    {
+        out<<l<<" "<<zj[l]<<endl;
+    }
+    in.close();
+    out.close();
+}
 
 int main()
 {
-    cout<<"1"<<endl;
+    srand((unsigned)time(0));
+
+    //====================================================================================================initial
+    for (int i=0;i<60;i++)
+    {
+        double testb;
+        testb=gaussrand()*0.1+1;
+        person[i]=testb;
+    }
+    for (int j=1;j<42625;j++)
+    {
+        double testc;
+        testc=gaussrand()*0.1+1;
+        item[j]=testc;
+    }
+    for (int k=1;k<42625;k++)
+    {
+        zj[k]=-1;
+    }
+    //======================================================================================================E²½
+    Initialize();
     return 0;
 }
